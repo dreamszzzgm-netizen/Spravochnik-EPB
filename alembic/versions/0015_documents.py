f0f1dcfd06c24348c051eb04e5210c8ca276835f
Timik Tim <dreamszzzgm@gmail.com>
2026-08-13 17:05:08 +0300

    feat: start documents migration

---
+++ alembic/versions/0015_documents.py
@@ -0,0 +1,10 @@
+"""documents foundation
+
+Revision ID: 0015_documents
+Revises: 0014_org_legal_form_fields
+"""
+
+revision = "0015_documents"
+down_revision = "0014_org_legal_form_fields"
+branch_labels = None
+depends_on = None
